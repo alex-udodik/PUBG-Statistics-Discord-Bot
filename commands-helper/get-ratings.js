9055a5268386163e3ba053e4c5f1f9a5567fafe5
alex-udodik <alex.udodik@wsu.edu>
2022-01-26 18:17:11 -0800

    Fixed checknames from cache. Implemented checknamesin database. Started checkNamesFromAPi

--- commands-helper/get-ratings.js
+++ commands-helper/get-ratings.js
@@ -8,42 +8,71 @@ class Ratings {
     }
 
     async getRatings() {
-        const obj = await _checkNamesInCache(this.names);
-        await _checkNamesInDatabase(obj);
+        var obj = await _checkNamesInCache(this.names);
+        obj = await _checkNamesInDatabase(obj);
     }
-
-
 }
-_checkNamesInCache = async (names) => {
-    var cachedNames = [];
 
+_checkNamesInCache = async (names) => {
+    var obj = {
+        accounts: [],
+        namesFromCache: 0
+    };
+
+    var namesFromCache = 0;
+
+    var accounts = [];
     await Promise.all(names.map(async name => {
         const id = await cache.verifyKey(name);
-        if (id !== null) { cachedNames.push({ name: name, accountId: id });}
-        else { cachedNames.push({ name: name, accountId: null });}
+        if (id !== null) { accounts.push({ name: name, accountId: id }); namesFromCache++; }
+        else { accounts.push({ name: name, accountId: null }); }
     }));
-    return cachedNames
+
+    obj.accounts = accounts
+    obj.namesFromCache = namesFromCache;
+
+    return obj;
 }
 
-_checkNamesInDatabase = async (accounts) => {
+_checkNamesInDatabase = async (obj) => {
+
+    if (obj.accounts.length === obj.namesFromCache) {
+        return obj;
+    }
 
     var querybuilder = new MongoQueryBuilder();
 
-    accounts.forEach(item => {
+    obj.accounts.forEach(item => {
         if (item.accountId === null) {
             const key = Object.keys(item)[0];
             const value = item.name;
             querybuilder.addQuery(key, value);
         }
-    })
-
+    });
 
     const query = querybuilder.build();
     console.log(query);
     const dbResults = await mongodb.findMany("PUBG", "Names", query);
-    await dbResults.forEach(doc => console.log(doc));
+    var namesFromMongoDB = 0;
 
+    await dbResults.forEach(doc => {
+        namesFromMongoDB++;
+        obj.accounts.forEach(account => {
+            if (doc.name === account.name.toLowerCase()) {
+                account.accountId = doc.accountId;
+            }
+        })
+    }
+    );
+
+    obj.namesFromMongoDB = namesFromMongoDB;
+    return obj;
 }
 
+_checkNamesFromAPI = async (obj) => {
+    if (obj.namesFromMongoDB === 0) { return obj; }
+
+    
+}
 
 module.exports = Ratings;
