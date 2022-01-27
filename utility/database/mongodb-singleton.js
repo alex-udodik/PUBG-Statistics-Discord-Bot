721032edc76e834734cc4a8d4209df1e4332869b
alex-udodik <alex.udodik@wsu.edu>
2022-01-26 21:15:34 -0800

    Removed hardcoding of username in connection string

--- utility/database/mongodb-singleton.js
+++ utility/database/mongodb-singleton.js
@@ -5,7 +5,7 @@ var MongoDBSingleton = (function () {
     var instance;
     var count = 0;
     function createInstance() {
-        const mongodbURI = `mongodb+srv://***REMOVED***:${process.env.MONGO_DB_PASSWORD}@clusterdev0.pcdo6.mongodb.net/test`;
+        const mongodbURI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@clusterdev0.pcdo6.mongodb.net/test`;
         var object = new MongoClient(mongodbURI);
         count += 1;
         return object;   
