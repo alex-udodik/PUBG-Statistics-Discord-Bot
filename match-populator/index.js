52f788bd6d648bda8f8d7ef440ceeabfac0aac6b
alex-udodik <alex.udodik@wsu.edu>
2023-05-06 18:31:25 -0700

    added some basic code to test function running at intervals

--- match-populator/index.js
+++ match-populator/index.js
@@ -1,6 +1,18 @@
 const mytime = require('./utility/time');
-
-(async () => {
-
+const api = require('../backend/utility/pubg/api');
+async function main() {
    console.log(mytime.getTimestamp() + " Started match farmer");
-})();
+
+   const endpoint = "https://api.pubg.com/shards/steam/samples?filter[createdAt-start]=2023-04-20T00%3A00%3A00Z";
+   const data = await api.fetchData(endpoint, 10000)
+
+   console.log(data);
+}
+
+async function run() {
+   await main();
+   setInterval(main, 200000)
+}
+
+
+run();
